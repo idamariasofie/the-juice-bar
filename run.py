cc0744c18f21e2b248d37e50701e65c2efd098f4
idamariasofie <>
2023-11-08 10:51:47 +0000

    Add get_size_data function to get size selection data from user

--- run.py
+++ run.py
@@ -14,9 +14,10 @@ SHEET = GSPREAD_CLIENT.open('the_juice_bar')
 
 def get_order_data():
     """
-    Get order value input from the user
+    Get juice order value input from the user
     """
     while True:
+        print("Welcome to The Juice Bar's order system")
         print("Please enter your juice of choice (1-5)")
         print("Then press Enter when you are ready.\n")
 
@@ -29,17 +30,35 @@ def get_order_data():
 
     return order_data
 
+def get_size_data():
+    """
+    Get juice size value input from the user
+    """
+    while True:
+        print("Here is our catalogue for sizes and prices")
+        print("Please enter the code for you juice size choice (S,M,L)")
+        print("Then press Enter when you are ready.\n")
+
+        data_str = input("Enter your order here:\n")
+        size_data = data_str.split(",")
+
+        if validate_data(size_data):
+            print("Thanks for your order")
+            break
+
+    return size_data
+
 def validate_data(value):
     """
     Converts all string values into integers.
     Raises ValueError if strings cannot be converted into integers.
-    Or if there are more then 1 value.
+    Or if there aren't exactly 1 value.
     """
     try:
         [int(value) for value in value]
         if len(value) != 1:
             raise ValueError(
-                f"Please enter a number 1-5, you entered {len(value)}")
+                f"Please enter a number between 1 and 5, you entered {len(value)}")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again\n")
         return False
@@ -55,6 +74,13 @@ def update_order_worksheet(data):
     order_worksheet.append_row(data)
     print("Your order was updated successfully\n")
 
+def main():
+    """
+    Run all program functions
+    """
 data = get_order_data()
 order_data = [int(num) for num in data]
 update_order_worksheet(order_data)
+size = get_size_data()
+
+main()
